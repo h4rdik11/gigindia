eaf3861f40b085f6723594710cf610752fe4d226
Hardik Chauhan <hardikchauhan@Appss-Mac-mini-4.local>
2020-11-28 20:21:27 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -14,12 +14,7 @@ function App() {
       ? JSON.parse(localStorage.getItem("upvotes"))
       : {}
   );
-
-  useEffect(() => {
-    callApi(state.page, state.search);
-  }, []);
-
-  const callApi = (page, search) => {
+  const callApi = (page = state.page, search = state.search) => {
     //hn.algolia.com/api/v1/
     fetch(
       `https://hn.algolia.com/api/v1/search_by_date?query=${search}&tags=story&page=${page}`
@@ -31,21 +26,21 @@ function App() {
         const upvoteObj = localStorage.getItem("upvotes")
           ? JSON.parse(localStorage.getItem("upvotes"))
           : {};
-        response.hits.map(({ objectID, points }) => {
-          if (
-            upvoteObj[objectID] &&
-            (!upvoteObj[objectID].done || upvoteObj[objectID].points < points)
-          ) {
-            upvoteObj[objectID] = {
-              done: false,
-              points,
-            };
-          }
+        response.hits.forEach(({ objectID, points }) => {
+          upvoteObj[objectID] = {
+            done: upvoteObj[objectID] ? upvoteObj[objectID].done : false,
+            points: upvoteObj[objectID] ? upvoteObj[objectID].points : points,
+            isHidden: upvoteObj[objectID] ? upvoteObj[objectID].isHidden : false,
+          };
         });
         setUpovtes(upvoteObj);
         setContracts(newContracts);
       });
   };
+
+  useEffect(() => {
+    callApi();
+  }, []);
 
   const gotourl = (url) => () => window.open(url, "_blank");
 
@@ -65,16 +60,31 @@ function App() {
   const handleSearchChange = (e) => {
     callApi(1, e.target.value);
   };
+
+  const localStorageContract = JSON.parse(localStorage.getItem("upvotes"));
+
+  const handleHide = (index, objectID) => (e) => {
+    const newContracts = [...contracts];
+    const newLS = { ...localStorageContract };
+    console.log(newLS);
+    newLS[objectID].isHidden = true;
+    localStorage.setItem("upvotes", JSON.stringify(newLS));
+    newContracts.splice(index, 1);
+    setContracts(newContracts);
+  };
+
   return (
     <div className="main__wrap">
       <div className="header">
         <div className="nav">
           <img src="https://news.ycombinator.com/y18.gif" alt="logo" />
-          <span className="nav-brand">Hacker News</span>
-          <div className="nav-items">
-            <a href="#">news</a>|<a href="#">past</a>|<a href="#">comments</a>|
-            <a href="#">ask</a>|<a href="#">show</a>|<a href="#">jobs</a>|
-            <a href="#">submit</a>
+          <div className="nav-item-wrap">
+            <span className="nav-brand">Hacker News</span>
+            <div className="nav-items">
+              <a href="/">news</a>|<a href="/">past</a>|<a href="/">comments</a>
+              |<a href="/">ask</a>|<a href="/">show</a>|<a href="/">jobs</a>|
+              <a href="/">submit</a>
+            </div>
           </div>
         </div>
         <div className="login-link">login</div>
@@ -85,36 +95,48 @@ function App() {
             { title, author, created_at, num_comments, url, objectID },
             index
           ) => {
-            return (
-              <div className="contract" key={`contract__${objectID}`}>
-                <span className="index">{index + 1 + (page - 1) * 20}.</span>
-                <img
-                  className="upvote_img"
-                  src={Upvote}
-                  alt="upvote"
-                  onClick={upVote(objectID)}
-                />
-                <div className="title_wrap">
-                  <span className="title">
-                    <span onClick={gotourl(url)} className="title-text">
-                      {title}
-                    </span>{" "}
-                    <small onClick={gotourl(url)} className="url-link">
-                      ({url})
-                    </small>
-                  </span>
-                  <div className="demographics">
-                    <span className="points">
-                      {upvotes[objectID] && upvotes[objectID].points} points
-                    </span>{" "}
-                    by <span className="username">{author}</span>
-                    <span className="time">2 hours ago</span> |{" "}
-                    <span className="hide">hide</span> |{" "}
-                    <span className="comments">{num_comments} comments</span>
+            if (
+              localStorageContract[objectID] &&
+              localStorageContract[objectID].isHidden
+            )
+              return null;
+            else
+              return (
+                <div className="contract" key={`contract__${objectID}`}>
+                  <span className="index">{index + 1 + (page - 1) * 20}.</span>
+                  <img
+                    className="upvote_img"
+                    src={Upvote}
+                    alt="upvote"
+                    onClick={upVote(objectID)}
+                  />
+                  <div className="title_wrap">
+                    <span className="title">
+                      <span onClick={gotourl(url)} className="title-text">
+                        {title}
+                      </span>{" "}
+                      <small onClick={gotourl(url)} className="url-link">
+                        ({url})
+                      </small>
+                    </span>
+                    <div className="demographics">
+                      <span className="points">
+                        {upvotes[objectID] && upvotes[objectID].points} points
+                      </span>{" "}
+                      by <span className="username">{author}</span>
+                      <span className="time">2 hours ago</span> |{" "}
+                      <span
+                        className="hide"
+                        onClick={handleHide(index, objectID)}
+                      >
+                        hide
+                      </span>{" "}
+                      |{" "}
+                      <span className="comments">{num_comments} comments</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
+              );
           }
         )}
       </div>
@@ -130,12 +152,16 @@ function App() {
       </div>
       <div className="footer_wrap">
         <div className="nav-items">
-          <a href="#">Guideline</a>|<a href="#">FAQ</a>|<a href="#">Lists</a>|
-          <a href="#" style={{ color: "#828282" }}>
-            API
+          <a href="https://news.ycombinator.com/newsguidelines.html">
+            Guideline
           </a>
-          |<a href="#">Security</a>|<a href="#">Legal</a>|
-          <a href="#">Apply to YC</a>|<a href="#">Contact</a>
+          |<a href="https://news.ycombinator.com/newsfaq.html">FAQ</a>|
+          <a href="https://news.ycombinator.com/lists">Lists</a>|
+          <a href="https://github.com/HackerNews/API">API</a>|
+          <a href="https://news.ycombinator.com/security.html">Security</a>|
+          <a href="http://www.ycombinator.com/legal/">Legal</a>|
+          <a href="http://www.ycombinator.com/apply/">Apply to YC</a>|
+          <a href="mailto:hn@ycombinator.com">Contact</a>
         </div>
         <div className="search_bar">
           <label>Search:</label>
